7c5150565c9f54db1d3d5e512d9450c4dc93ac5f
FernandoMSJ <fernando.sosaju@alumno.buap.mx>
2024-09-04 04:50:26 -0600

    Solucion inciso 3

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -38,6 +38,36 @@
     echo $b;
     echo'<br>';
 
-    
+    $a = "PHP5";
+    var_dump($a);
+    echo '<br>';
+
+    $z[]=&$a;
+    var_dump($z[0]);
+    echo'<br>';
+
+    $b = "5a version de PHP";
+    var_dump($b);
+    echo'<br>';
+
+    $c = $b*10;
+    var_dump($c);
+    echo'<br>';
+
+    $a .= $b;
+    var_dump($a);
+    echo'<br>';
+
+    $b *= $c;
+    var_dump($b);
+    echo'<br>';
+
+    $z[0] = "MySQL";
+    var_dump($z[0]);
+    echo'<br>';
+
+    //Contenido final de z
+    var_dump($z);
+    echo'<br>';
 
 ?>
