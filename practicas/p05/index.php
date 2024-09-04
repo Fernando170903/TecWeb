1cd0a51e901f7d63cf3c887debc45ff3207425cd
FernandoMSJ <fernando.sosaju@alumno.buap.mx>
2024-09-04 03:59:10 -0600

    Verificación de cuales son vairables y cuáles no

---
+++ practicas/p05/index.php
@@ -0,0 +1,23 @@
+<?php
+    $_myvar = 'dato1';
+    $_7var = 'dato2';
+    //myvar = 'dato3';
+    $myvar = 'dato4';
+    $var7 = 'dato5';
+    $_element1 = 'dato6';
+    //$house*5 = 'dato7';
+
+
+    echo $_myvar;
+    echo '<br>';
+    echo $_7var;
+    echo'<br>';
+    //echo myvar;
+    echo $myvar;
+    echo '<br>';
+    echo $var7;
+    echo '<br>';
+    echo $_element1;
+    echo '<br>';
+    //echo $house;
+?>
