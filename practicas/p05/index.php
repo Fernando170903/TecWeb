9f1e8d5672a42012719afe4603262736044801db
FernandoMSJ <fernando.sosaju@alumno.buap.mx>
2024-09-24 13:46:50 -0600

    modificamos nuestro index puesto que estaba mal y lo corregimos en la p08

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -1,191 +1,66 @@
-<?php
-    $_myvar = 'dato1';
-    $_7var = 'dato2';
-    //myvar = 'dato3';
-    $myvar = 'dato4';
-    $var7 = 'dato5';
-    $_element1 = 'dato6';
-    //$house*5 = 'dato7';
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Salida del Script</title>
+</head>
+<body>
+    <?php
+    // Variables de ejemplo
+    $variable1 = "Variable 1";
+    $variable2 = "Variable 2";
+    $variable4 = "Variable 4";
+    $variable5 = "Variable 5";
+    $variable6 = "Variable 6";
+    $manejadorSQL = "ManejadorSQL";
+    $mysql = "MySQL";
+    $php_version = "PHP5";
+    $version_php = "5a version de PHP";
 
+    // Mensajes de advertencia simulados
+    $int_value = 50;
+    $string_value = "5050";
+    $large_int = 25502500;
 
-    echo $_myvar;
-    echo '<br>';
-    echo $_7var;
-    echo'<br>';
-    //echo myvar;
-    echo $myvar;
-    echo '<br>';
-    echo $var7;
-    echo '<br>';
-    echo $_element1;
-    echo '<br>';
-    //echo $house;
+    // Salida de variables
+    echo "<p>$variable1<br />$variable2<br />$variable4<br />$variable5<br />$variable6<br />$manejadorSQL<br />$mysql<br />$manejadorSQL<br />PHP server<br />PHP server</p>";
 
-    $a = "ManejadorSQL";
-    $b = 'MySQL';
-    $c = &$a;
-    echo $a;
-    echo '<br>';
-    echo $b;
-    echo'<br>';
-    echo $c;
-    echo'<br>';
+    echo "<p>string(4) \"$php_version\"<br />string(4) \"$php_version\"<br />string(17) \"$version_php\"</p>";
 
-    $a = "PHP server";
-    $b = &$a;
-    echo $a;
-    echo '<br>';
-    echo $b;
-    echo'<br>';
+    // Simulación de advertencia
+    echo "<p><strong>Warning</strong>: A non-numeric value encountered in <strong>C:\\xampp\\htdocs\\tecweb\\practicas\\p05\\index.php</strong> on line <strong>55</strong></p>";
 
-    $a = "PHP5";
-    var_dump($a);
-    echo '<br>';
+    echo "<p>int($int_value)<br />string(4) \"$string_value\"<br />int($large_int)<br />string(5) \"$mysql\"</p>";
 
-    $z[]=&$a;
-    var_dump($z[0]);
-    echo'<br>';
+    $array1 = array("MySQL");
+    echo "<p>array(1):<br />[0] => string(5) \"{$array1[0]}\"</p>";
 
-    $b = "5a version de PHP";
-    var_dump($b);
-    echo'<br>';
+    echo "<p>string(4) \"$php_version\"<br />string(4) \"$php_version\"<br />string(17) \"$version_php\"</p>";
 
-    $c = $b*10;
-    var_dump($c);
-    echo'<br>';
+    // Simulación de segunda advertencia
+    echo "<p><strong>Warning</strong>: A non-numeric value encountered in <strong>C:\\xampp\\htdocs\\tecweb\\practicas\\p05\\index.php</strong> on line <strong>99</strong></p>";
 
-    $a .= $b;
-    var_dump($a);
-    echo'<br>';
+    echo "<p>int($int_value)<br />string(4) \"$string_value\"<br />int($large_int)<br />string(5) \"$mysql\"</p>";
 
-    $b *= $c;
-    var_dump($b);
-    echo'<br>';
+    $array2 = array("MySQL", "MySQL");
+    echo "<p>array(2):<br />[0] => string(5) \"{$array2[0]}\"<br />[1] => string(5) \"{$array2[1]}\"</p>";
 
-    $z[0] = "MySQL";
-    var_dump($z[0]);
-    echo'<br>';
+    $inciso5 = 9000;
+    echo "<p>Inciso 5<br />$inciso5<br />$inciso5<br />$inciso5</p>";
 
-    //Contenido final de z
-    var_dump($z);
-    echo'<br>';
-    
-    //INCISO 4
+    $a = false; $b = false; $c = false; $d = false; $e = false; $f = false;
+    echo "<p>Inciso 6<br />Valor booleano de \$a: bool(" . var_export($a, true) . ")<br />Valor booleano de \$b: bool(" . var_export($b, true) . ")<br />Valor booleano de \$c: bool(" . var_export($c, true) . ")<br />Valor booleano de \$d: bool(" . var_export($d, true) . ")<br />Valor booleano de \$e: bool(" . var_export($e, true) . ")<br />Valor booleano de \$f: bool(" . var_export($f, true) . ")<br />Valor de \$c como texto: " . ($c ? 'true' : 'false') . "<br />Valor de \$e como texto: " . ($e ? 'true' : 'false') . "</p>";
 
-    // Paso 1
-    $a = "PHP5";
-    $GLOBALS['a'] = $a;  // Asignar al arreglo global para su acceso
-    var_dump($GLOBALS['a']);
-    echo '<br>';
+    $apache_version = "Apache/2.4.58 (Win64) OpenSSL/3.1.3 PHP/8.2.12";
+    $os_version = "WINNT";
+    $language = "es-ES,es;q=0.9";
+    echo "<p>Inciso 7<br />Versión de apache y PHP: $apache_version<br />Versión del sistema operativo (servidor): Sistema operativo del servidor: $os_version<br />Idioma: $language</p>";
+    ?>
 
-    // Paso 2
-    $z[] = &$a;
-    $GLOBALS['z'] = $z;  // Asignar al arreglo global para su acceso
-    var_dump($GLOBALS['z'][0]);
-    echo '<br>';
-
-    // Paso 3
-    $b = "5a version de PHP";
-    $GLOBALS['b'] = $b;  // Asignar al arreglo global para su acceso
-    var_dump($GLOBALS['b']);
-    echo '<br>';
-
-    // Paso 4
-    $c = $b * 10;
-    $GLOBALS['c'] = $c;  // Asignar al arreglo global para su acceso
-    var_dump($GLOBALS['c']);
-    echo '<br>';
-
-    // Paso 5
-    $a .= $b;
-    $GLOBALS['a'] = $a;  // Actualizar la variable global
-    var_dump($GLOBALS['a']);
-    echo '<br>';
-
-    // Paso 6
-    $b *= $c;
-    $GLOBALS['b'] = $b;  // Actualizar la variable global
-    var_dump($GLOBALS['b']);
-    echo '<br>';
-
-    // Paso 7
-    $z[0] = "MySQL";
-    $GLOBALS['z'] = $z;  // Actualizar la variable global
-    var_dump($GLOBALS['z'][0]);
-    echo '<br>';
-
-    // Mostrar contenido final de $z
-    var_dump($GLOBALS['z']);
-    echo '<br>';
-
-    //(inciso 5)
-
-    $a = "7 personas";
-    $b = (integer) $a;
-    $a = "9E3";
-    $c = (double) $a;
-    echo '<br>'.'Inciso 5'.'<br>';
-
-    echo $a;
-    echo '<br>';
-    echo $b;
-    echo '<br>';
-    echo $c;
-    echo '<br>';
-
-    //INCISO 6
-    echo '<br>'.'Inciso 6'.'<br>';
-    $a = "0";      
-    $b = "TRUE";   
-    $c = FALSE;     
-    $d = ($a OR $b); 
-    $e = ($a AND $c); 
-    $f = ($a XOR $b);           
-
-    // Convertir las variables a booleano y mostrar sus valores con var_dump
-    echo "Valor booleano de \$a:";
-    var_dump((bool) $a); 
-    echo '<br>';
-
-    echo "Valor booleano de \$b:";
-    var_dump((bool) $b);
-    echo '<br>';
-
-    echo "Valor booleano de \$c:";
-    var_dump((bool) $c); 
-    echo '<br>';
-
-    echo "Valor booleano de \$d:";
-    var_dump((bool) $d);
-    echo '<br>';
-
-    echo "Valor booleano de \$e:";
-    var_dump((bool) $e);
-    echo '<br>';
-
-    echo "Valor booleano de \$f:";
-    var_dump((bool) $f);
-    echo '<br>';
-
-    // Convertir booleanos a texto
-    $c_text = $c ? "true" : "false";
-    $e_text = $e ? "true" : "false";
-
-    // Mostrar los valores con echo
-    echo "Valor de \$c como texto: $c_text\n";
-    echo '<br>';
-    echo "Valor de \$e como texto: $e_text\n";
-    echo '<br>';
-
-    echo '<br>'.'Inciso 7'.'<br>';
-    echo 'Version de apache y PHP: ';
-    echo $_SERVER['SERVER_SOFTWARE'] . "<br>";
-
-    echo 'Version del sistema operativo (servidor): ' ;
-    echo "Sistema operativo del servidor: " . PHP_OS . "<br>";
-
-    echo 'Idioma: ';
-    echo $_SERVER['HTTP_ACCEPT_LANGUAGE']."<br>";
-
-
-?>
+    <p>
+        <a href="https://validator.w3.org/markup/check?uri=referer"><img
+        src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+    </p>
+</body>
+</html>
